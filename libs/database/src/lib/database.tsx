0803bfb6e803a8ad5b852e6fc50453b48c6854e6
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2023-05-31 10:30:01 +0930

    🚀 feat: add new files and configurations for database library and server components
    🔧 chore(nx.json): update targetDefaults for lint to include eslint config files
    🔧 chore(nx.json): update namedInputs for production to exclude eslint config files
    🚀 chore(package.json): add prisma and swc dependencies
    🔧 chore(package.json): add eslint and typescript eslint dependencies
    🔧 chore(package.json): add eslint plugins and configs
    🔧 chore(tsconfig.base.json): add paths for database and database server modules

---
+++ libs/database/src/lib/database.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface DatabaseProps {}
+
+export function Database(props: DatabaseProps) {
+  return (
+    <div>
+      <h1>Welcome to Database!</h1>
+    </div>
+  );
+}
+
+export default Database;
