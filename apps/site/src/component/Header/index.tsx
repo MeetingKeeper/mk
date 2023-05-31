cacae526c1c66761f89c17f5789362648a815266
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2023-05-31 17:16:22 +0930

    🎨 style(Header/index.tsx): add login button to header 🐛 fix(Header/index.tsx): fix Link component not having a href attribute 🔒 chore(Header/index.tsx): add client-side rendering pragma to prevent SSR of signIn function

--- apps/site/src/component/Header/index.tsx
+++ apps/site/src/component/Header/index.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import Image from 'next/image';
 import Logo from "../../../public/assets/images/logo-192x192.png";
 import appConfig from "../../../app.config";
+import { signIn } from "next-auth/react";
 
 const Header = () => {
   return (
     <header className="js-page-header fixed top-0 z-20 w-full backdrop-blur transition-colors">
-      <div className="flex items-center px-6 py-6 xl:px-24 ">
+      <div className="flex items-center px-6 py-6 xl:px-24 w-full">
         <Link className="shrink-0" href="/" legacyBehavior>
           <a className="flex flex-row justify-center items-center">
             <div className="dark:block">
@@ -21,8 +24,11 @@ const Header = () => {
               {appConfig.name}
             </span>
           </a>
-
         </Link>
+        <div className="flex-grow flex-1"></div>
+        <div className="flex flex-row items-center space-x-4">
+          <button className="text-white outline" onClick={() => signIn()}>Login</button>
+        </div>
       </div>
     </header>
   )
