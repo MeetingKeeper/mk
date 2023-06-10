7c8b29274c14869d7ab764ea3fc349586eadd0b4
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2023-06-11 08:19:33 +0930

    🎨 style(summary/index.ts): format code with Prettier ✨ feat(summary/index.ts): refactor getSummary function to accept prompt parameter instead of combinePrompt and mapPrompt variables

--- packages/ai/summary/index.ts
+++ packages/ai/summary/index.ts
@@ -1,19 +1,29 @@
 import { loadSummarizationChain } from 'langchain/chains';
-import { OpenAI, PromptTemplate } from "langchain";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { Document } from "langchain/dist/document";
+import { OpenAI, PromptTemplate } from 'langchain';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { Document } from 'langchain/dist/document';
 
 const model = new OpenAI({
   temperature: 0.2,
   openAIApiKey: process.env.OPEN_AI_API_KEY,
 });
 
-export const getRecursiveDocs = async (text: string) : Promise<Document[]> => {
+export const getRecursiveDocs = async (text: string): Promise<Document[]> => {
   const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-  return  textSplitter.createDocuments([text]);
-}
+  return textSplitter.createDocuments([text]);
+};
 
-function getSummary(combinePrompt: PromptTemplate, mapPrompt: PromptTemplate, docs: Document[]) {
+function getSummary(prompt: string, docs: Document[]) {
+  const combinePrompt =
+    PromptTemplate.fromTemplate(`${prompt} delimited by triple backquotes.
+  Return your response in bullet points which covers the key points of the text.
+  {text}
+  SUMMARY:`);
+
+  const mapPrompt = PromptTemplate.fromTemplate(`${prompt}:
+  {text}
+  SUMMARY:`);
+
   const chain = loadSummarizationChain(model, {
     type: 'map_reduce',
     combinePrompt,
@@ -25,40 +35,16 @@ function getSummary(combinePrompt: PromptTemplate, mapPrompt: PromptTemplate, do
 }
 
 export const getInformativeSummary = async (docs: Document[]) => {
-  const combinePrompt = PromptTemplate.fromTemplate(`Provide an informative summary of the following conversation  delimited by triple backquotes.
-  Return your response in bullet points which covers the key points of the text.
-  {text}
-  SUMMARY:`);
-
-  const mapPrompt = PromptTemplate.fromTemplate(`Provide an informative summary of the following conversation:
-  {text}
-  SUMMARY:`);
-
-  return getSummary(combinePrompt, mapPrompt, docs);
+  const prompt = 'Provide an informative summary of the following conversation';
+  return getSummary(prompt, docs);
 };
 
 export const getDecisionMakingSummary = async (docs: Document[]) => {
-  const combinePrompt = PromptTemplate.fromTemplate(`Analyze the decision-making process in the conversation and identify key moments where decisions were made or discussed delimited by triple backquotes.
-  Return your response in bullet points which covers the key points of the text.
-  {text}
-  SUMMARY:`);
-
-  const mapPrompt = PromptTemplate.fromTemplate(`Analyze the decision-making process in the following conversation and identify key moments where decisions were made or discussed:
-  {text}
-  SUMMARY:`);
-
-  return getSummary(combinePrompt, mapPrompt, docs);
+  const prompt = 'Analyze the decision-making process in the conversation and identify key moments where decisions were made or discussed';
+  return getSummary(prompt, docs);
 };
 
 export const getActionOrientedSummary = async (docs: Document[]) => {
-  const combinePrompt = PromptTemplate.fromTemplate(`Extract the follow-up action items from the conversation delimited by triple backquotes.
-  Return your response in bullet points which covers the key points of the text.
-  {text}
-  ACTION-ORIENTED SUMMARY:`);
-
-  const mapPrompt = PromptTemplate.fromTemplate(`Extract the follow-up action items from the conversation:
-  {text}
-  ACTION-ORIENTED SUMMARY:`);
-
-  return getSummary(combinePrompt, mapPrompt, docs);
+  const prompt = 'Extract the follow-up action items from the conversation';
+  return getSummary(prompt, docs);
 };
