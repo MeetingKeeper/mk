1593a112f5776bb5d171338f4846f126412ba7aa
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2023-07-03 08:56:55 +0930

    🎨 style(Layout): update layout structure and add dark mode support 🔨 refactor(Dashboard/form): remove unused import 🎨 style(globals.css): add main selector to ensure full height and width

--- apps/desktop/renderer/components/Layouts/index.tsx
+++ apps/desktop/renderer/components/Layouts/index.tsx
@@ -3,7 +3,13 @@ import { ThemeProvider } from '@pm/ui';
 export default function Layout({ children }) {
   return (
     <ThemeProvider>
-      <main>{children}</main>
+      <div className="dark h-full" dir="ltr">
+        <div className="dark:bg-black flex h-full">
+         <div className="dark:bg-black bg-black flex min-h-screen flex-col items-center justify-between h-full w-full">
+           <main>{children}</main>
+         </div>
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
